61eaa8e329b57596e9c2ead2c4b8de1adca2e4d1
13Characters_ <benjt0523@gmail.com>
2025-12-02 22:20:28 -0800

    improve day 2 golf

--- day2/day2_golf.py
+++ day2/day2_golf.py
@@ -1,6 +1,6 @@
 def part_1(fp):
-    return sum(sum(i for i in range(r[0], r[1]) if str(i)[:len(str(i))//2] == str(i)[len(str(i))//2:]) for r in (tuple(int(x) for x in y.split("-")) for y in open(fp).read().strip().split(",")))
+    return sum(sum(i for i in range(*r)if(s:=str(i))[:len(s)//2]*2==s)for r in(tuple(map(int,y.split("-")))for y in open(fp).read().strip().split(",")))
 def part_2(fp):
-    return sum(sum(i for i in range(r[0], r[1]) if any(len(str(i)) % j == 0 and i % sum([10**(j*k) for k in range(len(str(i)) // j)]) == 0 for j in range(1, len(str(i))))) for r in (tuple(int(x) for x in y.split("-")) for y in open(fp).read().strip().split(",")))
+    return sum(sum(i for i in range(*r)if((s:=str(i))*2).find(s,1)!=len(s))for r in(tuple(map(int,y.split("-")))for y in open(fp).read().strip().split(",")))
 print(part_1("input"))
 print(part_2("input"))
