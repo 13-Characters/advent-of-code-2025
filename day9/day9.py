e02b6bf027340763ff3d794df92be510592134a9
13Characters_ <benjt0523@gmail.com>
2025-12-09 13:21:44 -0800

    optimize part 2

--- day9/day9.py
+++ day9/day9.py
@@ -9,26 +9,6 @@ def get_matrix(fp):
            range(len(points)) if i<j}
     return points, mat
 
-def is_inside(point_1, loop):
-    # Implementation of the winding number algorithm
-    winding_number = 0
-    diff_prev = False
-    for point_2 in loop:
-        diff = tuple(y-x for x,y in zip(point_1, point_2))
-        if diff_prev and diff[0]>0:
-            if diff_prev[1]*diff[1] < 0:
-                if diff_prev[1] < 0:
-                    winding_number += 2
-                else:
-                    winding_number -= 2
-            if diff_prev[1]*diff[1] == 0:
-                if diff_prev[1] < 0 or diff[1] > 0:
-                    winding_number += 1
-                elif diff_prev[1] > 0 or diff[1] < 0:
-                    winding_number -= 1
-        diff_prev = diff
-    return winding_number != 0
-
 def make_compression(points):
     x_values = list(sorted(set(point[0] for point in points)))
     y_values = list(sorted(set(point[1] for point in points)))
@@ -60,23 +40,6 @@ def make_compression(points):
 
 def part_1(fp):
     return max(get_matrix(fp)[1].values())
-
-def part_2(fp):
-    points, mat = get_matrix(fp)
-    keys_to_check = sorted(mat.keys(),key=lambda x: mat[x], reverse=True)
-    x_values = [point[0] for point in points]
-    y_values = [point[1] for point in points]
-    # Computing this takes O(n^3) time
-    outside_points = [(x, y) for x in x_values for y in y_values if not is_inside((x, y), points)]
-    for key in keys_to_check:
-        i,j = key
-        a,b = points[i], points[j]
-        x_left, x_right = min(a[0], b[0]), max(a[0], b[0])
-        y_up, y_down = min(a[1], b[1]), max(a[1], b[1])
-        good = not any(point for point in outside_points if x_left<=point[0]<=x_right and y_up<=point[1]<=y_down)
-        if good:
-            print(a, b)
-            return area(a,b)
 
 def part_2_fast(fp):
     points_uncompressed, mat = get_matrix(fp)
