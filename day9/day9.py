fc9ad53095ce3e6ff7e29d2227f5e1e858d4214e
13Characters_ <benjt0523@gmail.com>
2025-12-09 12:41:09 -0800

    finish day 9, but slow

--- day9/day9.py
+++ day9/day9.py
@@ -24,10 +24,38 @@ def is_inside(point_1, loop):
             if diff_prev[1]*diff[1] == 0:
                 if diff_prev[1] < 0 or diff[1] > 0:
                     winding_number += 1
-                else:
+                elif diff_prev[1] > 0 or diff[1] < 0:
                     winding_number -= 1
         diff_prev = diff
     return winding_number != 0
+
+def make_compression(points):
+    x_values = list(sorted(set(point[0] for point in points)))
+    y_values = list(sorted(set(point[1] for point in points)))
+    size = (2*len(x_values)+1, 2*len(y_values)+1)
+    def search_range(a, values):
+        if a < values[0]:
+            return 0
+        elif a > values[-1]:
+            return len(values)*2
+        i,j = 0,len(values)-1
+        while abs(j-i) > 1:
+            if values[i] < a < values[j//2]:
+                j = j//2
+            else:
+                i = j//2
+        return i+j + 1
+    def compression(x,y):
+        if x in x_values:
+            x_new = x_values.index(x)*2 + 1
+        else:
+            x_new = search_range(x, x_values)
+        if y in y_values:
+            y_new = y_values.index(y)*2 + 1
+        else:
+            y_new = search_range(y, y_values)
+        return x_new,y_new
+    return compression, size
 
 
 def part_1(fp):
@@ -36,39 +64,53 @@ def part_1(fp):
 def part_2(fp):
     points, mat = get_matrix(fp)
     keys_to_check = sorted(mat.keys(),key=lambda x: mat[x], reverse=True)
+    x_values = [point[0] for point in points]
+    y_values = [point[1] for point in points]
+    # Computing this takes O(n^3) time
+    outside_points = [(x, y) for x in x_values for y in y_values if not is_inside((x, y), points)]
     for key in keys_to_check:
         i,j = key
         a,b = points[i], points[j]
-        if (9,5) in [a,b]:
-            pass
         x_left, x_right = min(a[0], b[0]), max(a[0], b[0])
         y_up, y_down = min(a[1], b[1]), max(a[1], b[1])
-
-        for p1,p2 in zip(points, points[1:]+[points[0]]):
-            dx,dy = tuple(y-x for x,y in zip(p1,p2))
-            x,y = p2
-            if y_up<y<y_down:
-                if dx>0:
-                    # Cross the left edge of rectangle
-                    if p2[0]>x_left and p1[0]<x_left:
-                        break
-                if dx<0:
-                    # Cross the right edge of rectangle
-                    if p2[0]<x_right:
-                        break
-            if x_left<x<x_right:
-                if dy>0:
-                    # Cross the upper edge of rectangle
-                    if p2[1]>y_up and p1[1]<y_up:
-                        break
-                if dy<0:
-                    # Cross the lower edge of rectangle
-                    if p2[1]<y_down and p1[1]>y_up:
-                        break
-        else:
-            print(a,b)
+        good = not any(point for point in outside_points if x_left<=point[0]<=x_right and y_up<=point[1]<=y_down)
+        if good:
+            print(a, b)
             return area(a,b)
+
+def part_2_fast(fp):
+    points_uncompressed, mat = get_matrix(fp)
+    keys_to_check = sorted(mat.keys(), key = lambda x: mat[x], reverse = True)
+    compression,size = make_compression(points_uncompressed)
+    points = [compression(*p) for p in points_uncompressed]
+    inside={(i,j): False for i in range(size[0]) for j in range(size[1])}
+
+    # Draw the outline
+    for p1,p2 in zip(points,points[1:]+[points[0]]):
+        x_min, x_max = min(p1[0], p2[0]), max(p1[0], p2[0])
+        y_min, y_max = min(p1[1], p2[1]), max(p1[1], p2[1])
+        for x in range(x_min,x_max+1):
+            for y in range(y_min, y_max + 1):
+                inside[x,y] = True
+
+    # Fill the inside
+    for y in range(size[1]):
+        fill = False
+        for x in range(0, size[1] - 1):
+            if fill:
+                inside[x,y] = True
+            if inside[x,y]:
+                fill = not inside[x+1,y]
+
+    for key in keys_to_check:
+        i,j = key
+        a,b = points[i], points[j]
+        x_left, x_right = min(a[0], b[0]), max(a[0], b[0])
+        y_up, y_down = min(a[1], b[1]), max(a[1], b[1])
+        for x in x_values:
+            for y in y_values:
+                pass
 
 if __name__ == "__main__":
     print(part_1("input"))
-    print(part_2("example_input"))
+    print(part_2("input"))
